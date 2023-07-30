ea811f960fb8ceef56d44bf1257e5e61a649e8a3
shuang <shuang.lixx@qq.com>
2023-07-30 12:11:14 +0800

    fix：Hydration failed ，and sidebar selected

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -9,6 +9,16 @@ import Sidebar from "./Sidebar";
 
 
 export const MobileSidebar = () => {
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(()=>{
+    setIsMounted(true)
+  },[])
+
+  if(!isMounted){
+    return null;
+  }
   return (
     <Sheet>
       <SheetTrigger>
